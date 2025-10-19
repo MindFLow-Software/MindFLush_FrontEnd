d4696e8efa9d8a7595fc6b4af20f2308471595ce
PauloStraforini <paulostraforini@gmail.com>
2025-10-19 18:12:46 -0300

    update routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
-import { PatientsList } from './pages/app/patients/new-patients/patients-list'
+import { PatientsList } from './pages/app/patients/patients-list/patients-list'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { NotFound } from './pages/404'
 

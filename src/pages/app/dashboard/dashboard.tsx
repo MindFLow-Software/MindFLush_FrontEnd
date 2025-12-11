0db0d375432b81e0a0c9edcbd954feb71c4e7f89
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-12-11 14:03:29 -0300

    update

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -55,7 +55,7 @@ export function Dashboard() {
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                     <PatientsCountCard startDate={startDate} endDate={endDate} />
                     <PatientsAmountCard />
-                    <MonthPatientsAmountCard startDate={startDate} endDate={endDate} />
+                    {/* <MonthPatientsAmountCard startDate={startDate} endDate={endDate} /> */}
                 </div>
             </div>
 
